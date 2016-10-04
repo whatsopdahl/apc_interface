7b16a3698ab80aa1eec7ba5c08ecec709e50b22b
mcneky01 <kjmcneese@gmail.com>
2016-10-03 22:32:02 -0500

    basic dashboard drafted

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 var app = angular.module('CourseProposalApp', []);
+
+app.directive("dashHeader", function() {
+    return {
+        restrict: "E",
+        templateUrl: "../dashboard/dash-header.html"
+    };
+});
--- js/dashboardModule.js
+++ js/dashboardModule.js
@@ -1,3 +1,2 @@
 app.controller('dashboardCtrl', function($scope) {
-    $scope.shouldHide = false;
 });
