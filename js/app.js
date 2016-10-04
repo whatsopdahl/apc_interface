50bf6d15cbf0a6c9546b477dbd1780eefdcacaae
mcneky01 <kjmcneese@gmail.com>
2016-10-04 13:17:49 -0500

    refactoring

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 var app = angular.module('CourseProposalApp', []);
 
-app.directive("dashHeader", function() {
+app.directive("navbar", function() {
     return {
         restrict: "E",
-        templateUrl: "../dashboard/dash-header.html"
+        templateUrl: "../html/navbar.html"
+    };
+});
+
+app.directive("dashboard", function() {
+    return {
+        restrict: "E",
+        templateUrl: "../html/dashboard.html"
+    };
+});
+
+app.directive("courseList", function() {
+    return {
+        restrict: "E",
+        templateUrl: "../html/course-list.html"
     };
 });
