ac85a7e34203d1334e4f42daac87ee0bc8f6c0ab
Aidan <aidanschmitt47@gmail.com>
2016-10-11 13:45:46 -0500

    added a run script for the mongodb backend

---
+++ database/run_backend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#	Start the MongoDB service
+#sudo service mongod start
+
+#	Load data included in mock_data.js into table apcdata
+mongo apcdata mock_data.js
+
+#	Start the java backend
+#java -jar ApcBackend-0.1-jar-with-dependencies.jar
+java -jar ApcBackend/target/ApcBackend-0.1-jar-with-dependencies.jar
+ 
+
