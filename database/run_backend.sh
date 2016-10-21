105f92721037e3502accb1c041de2db27628224e
Aidan <aidanschmitt47@gmail.com>
2016-10-20 20:14:20 -0500

    changed script to new jar version (note: .js file needs to be changed to use real data

--- database/run_backend.sh
+++ database/run_backend.sh
@@ -8,6 +8,6 @@ mongo apcdata mock_data.js
 
 #	Start the java backend
 #java -jar ApcBackend-0.1-jar-with-dependencies.jar
-java -jar ApcBackend/target/ApcBackend-0.1-jar-with-dependencies.jar
+java -jar ApcBackend/target/ApcBackend-1.0_1-jar-with-dependencies.jar
  
 
