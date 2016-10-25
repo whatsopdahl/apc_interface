0afbbfda85abcca1b37d630595470a7b5332865c
mcneky01 <kjmcneese@gmail.com>
2016-10-24 19:31:32 -0500

    added headers to response

--- database/ApcBackend/src/main/java/com/apc_interface/apc_backend/ApcHandler.java
+++ database/ApcBackend/src/main/java/com/apc_interface/apc_backend/ApcHandler.java
@@ -24,9 +24,9 @@ import org.bson.types.ObjectId;
  * Handles all HTTP requests. When HTTP requests are made, they are sent by
  * the {@link ApcBackend} file here, and get parsed and responded to.
  * <p>
- * This class also provides interfacing with a <code>MongoDB</code> database, 
+ * This class also provides interfacing with a <code>MongoDB</code> database,
  * which is responsible for maintaining and serving all required data.
- * 
+ *
  * @author Aidan Schmitt
  * @version 1.0_1
  * @since 1.0_1
@@ -35,37 +35,37 @@ import org.bson.types.ObjectId;
  * @see MongoClient
  */
 public class ApcHandler implements HttpHandler{
-    
+
     /**
      * The name of the database stored in the MongoDB server.
      */
     private static final String DB_NAME = "apcdata";
-    
+
     /**
      * String finals for the names of the different collections in the database.
      */
     private static final String COLLECTION_COURSES = "courses";
     private static final String COLLECTION_USERS = "users";
     private static final String COLLECTION_PROPOSALS = "proposals";
-    
+
     /**
      * An object representation of the MongoDB database.
      */
     private MongoDatabase db;
-    
+
     /**
      * <code>String</code> finals for HTML header setting.
      * Note: <code>HEADER_CONTENT_TYPE might not be needed.
      */
     private static final String HEADER_ALLOW = "Allow";
     private static final String HEADER_CONTENT_TYPE = "Content-Type";
-    
+
     /**
      * Character set for encoding/decoding HTTP requests.
      * Note: This might not be needed.
      */
     private static final Charset CHARSET = StandardCharsets.UTF_8;
-    
+
     /**
      * HTTP status codes.
      * <p>
@@ -85,13 +85,13 @@ public class ApcHandler implements HttpHandler{
     private static final int STATUS_BAD_REQUEST = 400;
     private static final int STATUS_NOT_FOUND = 404;
     private static final int STATUS_METHOD_NOT_ALLOWED = 405;
-    
+
     /**
      * Length to put in the HTTP response headers if the message body has no
      * contents.
      */
     private static final int NO_RESPONSE_LENGTH = -1;
-    
+
     /**
      * <code>String</code> finals for HTTP methods.
      */
@@ -99,7 +99,7 @@ public class ApcHandler implements HttpHandler{
     private static final String METHOD_POST = "POST";
     private static final String METHOD_OPTIONS = "OPTIONS";
     private static final String ALLOWED_METHODS = METHOD_GET + "," + METHOD_POST + "," + METHOD_OPTIONS;
-    
+
     /**
      * Initializes the reference to the database hosted on the MongoDB server.
      */
@@ -107,38 +107,38 @@ public class ApcHandler implements HttpHandler{
         MongoClient client = new MongoClient();
         db = client.getDatabase(DB_NAME);
     }
-    
+
     /**
      * Handles the HTTP requests. Switches based on method type, and handles
      * each method differently. If an invalid method is used, it sends a 405
      * error and tells the client which methods are available.
      * <p>
-     * GET request format: 
+     * GET request format:
      *      apc.url.luther.edu/{@link ApcBackend.CONTEXT_PATH}?q=query[&u=user]
      *      query can be: courses (get all courses)
      *                    proposals (get all proposals)
      *                    users (get all users, or get a specific user with u=)
      *                    recent (get recently viewed)
      *      user should be username
-     * 
+     *
      * POST request format (in body of http request):
      *      q=query&[extra querys &]d=json
-     *      
+     *
      *      query can be: create (create a proposal)
      *                    edituser (edit a user)
      *                    save (edit a proposal)
      *                    delete (delete a proposal)
-     * 
+     *
      * Note: more methods can be easily added to either GET or POST
-     * 
-     * Each time a request is handled it is surrounded in a 
+     *
+     * Each time a request is handled it is surrounded in a
      * <code>try/catch</code> block, in order to ensure the server's continued
      * execution no matter if a bad request is processed. In this event, the
      * server simply responds with an HTTP error code and continues serving new
      * requests.
-     * 
+     *
      * TODO implement HTTP PUT method as an alternative for POSTing some things
-     * 
+     *
      * @param t the {@link HttpExchange} being handled
      * @throws IOException if sending either response headers or message body
      * is invalid
@@ -147,14 +147,16 @@ public class ApcHandler implements HttpHandler{
     public void handle(HttpExchange t) throws IOException {
         try{
             final Headers headers = t.getResponseHeaders();
+            headers.put('Access-Control-Allow-Origin', 'http://localhost:8000');
+            headers.put('Access-Control-Allow-Credentials', 'true');
             final String requestMethod = t.getRequestMethod().toUpperCase();
             switch(requestMethod){
                 case METHOD_GET:
                     Map<String, String> params = parseQuery(t.getRequestURI().getQuery());
-                    
+
                     String response;
                     int status;
-                    
+
                     switch(params.get("q")){
                         case "courses":
                             try{
@@ -201,7 +203,7 @@ public class ApcHandler implements HttpHandler{
                             response = "";
                             break;
                     }
-                    
+
                     t.sendResponseHeaders(status, response.length());
                     OutputStream os = t.getResponseBody();
                     os.write(response.getBytes());
@@ -210,20 +212,20 @@ public class ApcHandler implements HttpHandler{
                 case METHOD_POST:
                     InputStreamReader isr = new InputStreamReader(t.getRequestBody(), "utf-8");
                     BufferedReader br = new BufferedReader(isr);
-                    
+
                     int b;
                     StringBuilder buf = new StringBuilder(512);
                     while((b = br.read()) != -1){
                         buf.append((char) b);
                     }
-                    
+
                     br.close();
                     isr.close();
-                    
+
                     params = parseQuery(buf.toString());
-                    
+
                     Document doc = Document.parse(params.get("d"));
-                    
+
                     switch(params.get("q")){
                         case "create":
                             try{
@@ -276,12 +278,12 @@ public class ApcHandler implements HttpHandler{
                             response = "{status: not found}";
                             break;
                     }
-                    
+
                     t.sendResponseHeaders(status, response.length());
                     os = t.getResponseBody();
                     os.write(response.getBytes());
                     os.close();
-                    
+
                     break;
                 case METHOD_OPTIONS:
                     headers.set(HEADER_ALLOW, ALLOWED_METHODS);
@@ -301,18 +303,18 @@ public class ApcHandler implements HttpHandler{
      * Handle HTTP GET request for recently viewed proposals or courses.
      */
     private String getRecentlyViewed(String user){
-        
+
         final StringBuilder json = new StringBuilder();
         json.append("{");
-       
+
         FindIterable iterable = db.getCollection(COLLECTION_USERS).find(eq("name", user));
-        
+
         iterable.forEach(new Block<Document>(){
             @Override
             public void apply(final Document document){
                 ArrayList<String> list = null;
                 list = document.get("recent", ArrayList.class);
-                
+
                 for (int i = 0; i < list.size(); i++){
                     FindIterable iter = db.getCollection(COLLECTION_PROPOSALS).find(eq("_id", new ObjectId(list.get(i))));
                     iter.forEach(new Block<Document>(){
@@ -321,7 +323,7 @@ public class ApcHandler implements HttpHandler{
                             json.append(doc.toJson());
                         }
                     });
-                    
+
                     iter = db.getCollection(COLLECTION_COURSES).find(eq("_id", new ObjectId(list.get(i))));
                     iter.forEach(new Block<Document>(){
                         @Override
@@ -332,17 +334,17 @@ public class ApcHandler implements HttpHandler{
                 }
             }
         });
-        
+
         json.append("}");
-                
+
         return json.toString();
     }
 
     /**
-     * Handle HTTP GET request for all documents in a given collection. 
+     * Handle HTTP GET request for all documents in a given collection.
      * Initiates a database query that returns all <code>Document</code>s in a
      * given collection, and parses them into JSON text.
-     * 
+     *
      * @param collection the collection to retrieve documents from
      * @return a <code>JSON</code> representation of all courses in the database
      */
@@ -359,32 +361,32 @@ public class ApcHandler implements HttpHandler{
 
         return json.toString();
     }
-    
+
     /**
      * Handle HTTP GET request for a specific user. Note that this will return
-     * all users with the same username, so care should be taken to avoid 
+     * all users with the same username, so care should be taken to avoid
      * duplicate usernames.
-     * 
+     *
      * @param user the username of the intended user
      * @return a JSON formatted string of the user's data
      */
     private String getUser(String user){
         final StringBuilder json = new StringBuilder();
         FindIterable iterable = db.getCollection(COLLECTION_USERS).find(eq("name", user));
-        
+
         iterable.forEach(new Block<Document>() {
            @Override
            public void apply(final Document document){
                json.append(document.toJson());
            }
         });
-        
+
         return json.toString();
     }
-    
+
     /**
      * Parses an HTTP GET query into a map (dictionary) of params:values.
-     * 
+     *
      * @param query a string containing the GET query
      * @return a map of key-value pairings of parameters and values
      */
