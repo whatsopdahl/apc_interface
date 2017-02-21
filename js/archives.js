fc344de88744fe42075b3b5c778772e6a863df4a
mcneky01 <kjmcneese@gmail.com>
2017-02-21 14:23:32 -0600

    finished some archive interface features

--- js/archives.js
+++ js/archives.js
@@ -1,8 +1,23 @@
 var app = angular.module("CourseProposalApp");
 
 app.controller("archivesCtrl", archivesCtrl);
+app.directive("archivesModal", archivesModal);
 
-archivesCtrl.$inject = ["$scope", "$rootScope", "dataSrv"];
-function archivesCtrl($scope, $rootScope, dataSrv) {
 
+archivesCtrl.$inject = ["$scope", "$rootScope"];
+function archivesCtrl($scope, $rootScope) {
+    $scope.openArchiveModal= function() {
+        console.log("HELLLLOOOOOOOO");
+        var theModal;
+
+        theModal = angular.element("#show-archives");
+		theModal.modal("show");
+	}
+}
+
+function archivesModal() {
+	return {
+		restrict : "E",
+		templateUrl : "templates/course-archive.html"
+	}
 }
